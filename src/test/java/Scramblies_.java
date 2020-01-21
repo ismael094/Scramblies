8909b246bb79f5df04e1b5d67adb1254a218d057
ismael094 <ismaelpitufo@gmail.com>
2020-01-21 16:27:15 +0000

    Second test: red and green test

--- src/test/java/Scramblies_.java
+++ src/test/java/Scramblies_.java
@@ -1,5 +1,10 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Scramblies_ {
@@ -8,7 +13,19 @@ public class Scramblies_ {
         assertThat(scramble("hola","hola")).isEqualTo(true);
     }
 
+    @Test
+    public void with_portion_of_str2_in_str1_should_return_true() {
+        assertThat(scramble("cedewaraaossoqqyt","codewars")).isEqualTo(true);
+    }
+
     private boolean scramble(String str1, String str2) {
-        return str1.equals(str2);
+        if (str1.equals(str2))
+            return true;
+        Stream<Character> characterStream = str2.chars()
+                .filter(i -> !str1.contains((char) i + ""))
+                .mapToObj(i -> (char) i);
+        int size = characterStream.collect(Collectors.toList()).size();
+        return size == 0;
+
     }
 }
