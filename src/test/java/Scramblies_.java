1aff3f66f1ac8ea52f05b9d4c9c357f40a712bc9
ismael094 <ismaelpitufo@gmail.com>
2020-01-21 16:31:28 +0000

    Second test: refactor

--- src/test/java/Scramblies_.java
+++ src/test/java/Scramblies_.java
@@ -14,18 +14,16 @@ public class Scramblies_ {
     }
 
     @Test
-    public void with_portion_of_str2_in_str1_should_return_true() {
+    public void with_portion_of_str1_in_str2_should_return_true() {
         assertThat(scramble("cedewaraaossoqqyt","codewars")).isEqualTo(true);
     }
 
     private boolean scramble(String str1, String str2) {
-        if (str1.equals(str2))
-            return true;
-        Stream<Character> characterStream = str2.chars()
+        Long collect = str2.chars()
                 .filter(i -> !str1.contains((char) i + ""))
-                .mapToObj(i -> (char) i);
-        int size = characterStream.collect(Collectors.toList()).size();
-        return size == 0;
+                .mapToObj(i -> (char) i)
+                .collect(Collectors.counting());
+        return collect == 0;
 
     }
 }
